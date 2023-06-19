d84bf5dc15fe02ea9e2532113aeb1d1a57ab4acf
Jelle <j.p.veldmaat@student.utwente.nl>
2023-06-19 18:29:59 +0200

    fixed analyse

--- analyse.py
+++ analyse.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import os
+
 
 
 experiments_array = [
@@ -26,19 +28,31 @@ wifi_results = []
 
 for experiment in experiments_array:
 
-    df = pd.read_csv(f'experiments/{experiment[1]}/1/EC_94_CB_49_B2_F0_STA.csv', header=None)
+    result_amounts = []
+
+    for file_name in os.listdir(f"experiments/{experiment[1]}/1"):
+        if "bad_" not in file_name:
+            df = pd.read_csv(f'experiments/{experiment[1]}/1/{file_name}', header=None)
+            result_amounts.append(df.shape[0])
 
     # print(df.to_string())
+    print(experiment[2])
     if experiment[0] == "serial":
-        serial_results.append([experiment[2]*15, df.shape[0]])
+        serial_results.append([experiment[2], sum(result_amounts)/len(result_amounts)/15])
     else:
-        wifi_results.append([experiment[2]*15, df.shape[0]])
+        wifi_results.append([experiment[2], sum(result_amounts)/len(result_amounts)/15])
 
 print(serial_results)
+
+x_axis = list(zip(*serial_results))[0]
+
 plt.plot(*zip(*serial_results), label="serial")
 plt.plot(*zip(*wifi_results), label="wifi")
+plt.plot(x_axis, x_axis, label="ideal")
+plt.xticks(x_axis)
+plt.grid()
 plt.legend()
-plt.title('Random Figure')
-plt.xlabel('X-Axis')
-plt.ylabel('Y-Axis')
+plt.title('Hz Actual vs Hz Setting')
+plt.xlabel('Hz Setting')
+plt.ylabel('Hz Actual')
 plt.show()
