6c54b7f856cf1334fa6ae0a0e7edd3d25d9417df
Jelle <j.p.veldmaat@student.utwente.nl>
2023-06-20 12:35:20 +0200

    plots added

--- plot_CV.py
+++ plot_CV.py
@@ -8,25 +8,22 @@ df = pd.read_csv('results.csv')
 station_amount = df['station_amount']
 transfer_type = df['transfer_type']
 hz_setting = df['hz_setting']
-mean_time = df['mean_time_between_packets']
-expected_time = df['expected_time_between_packets']
-
+CV_time = df['CV_time_between_packets']
 
 # Set up the plot
 plt.figure(figsize=(10, 6))
 plt.xlabel('hz_setting')
-plt.ylabel('mean_time_between_packets')
+plt.ylabel('CV_time_between_packets')
 
 # Group the data by station_amount and transfer_type
 grouped_data = df.groupby(['station_amount', 'transfer_type'])
 
 # Plot each group as a line
 for (station, transfer), group in grouped_data:
-    plt.plot(group['hz_setting'], group['mean_time_between_packets'], marker='o', label=f'{station}, {transfer}')
-
+    plt.plot(group['hz_setting'], group['CV_time_between_packets'], marker='o', label=f'{station}, {transfer}')
 
 # Add the ideal line
-plt.plot(hz_setting, expected_time, color='red', linestyle='--', label='Ideal')
+plt.axhline(y=0, color='red', linestyle='--', label='Ideal')
 
 
 plt.grid(True)
--- plot_timing_ratio.py
+++ plot_timing_ratio.py
@@ -8,25 +8,29 @@ df = pd.read_csv('results.csv')
 station_amount = df['station_amount']
 transfer_type = df['transfer_type']
 hz_setting = df['hz_setting']
-mean_time = df['mean_time_between_packets']
 expected_time = df['expected_time_between_packets']
+mean_time = df['mean_time_between_packets']
 
+# Calculate the ratio of mean_time divided by expected_time
+ratio = mean_time / expected_time
 
 # Set up the plot
 plt.figure(figsize=(10, 6))
 plt.xlabel('hz_setting')
-plt.ylabel('mean_time_between_packets')
+plt.ylabel('mean_time / expected_time')
+
+# Enable grid
+plt.grid(True)
 
 # Group the data by station_amount and transfer_type
 grouped_data = df.groupby(['station_amount', 'transfer_type'])
 
 # Plot each group as a line
 for (station, transfer), group in grouped_data:
-    plt.plot(group['hz_setting'], group['mean_time_between_packets'], marker='o', label=f'{station}, {transfer}')
-
+    plt.plot(group['hz_setting'], group['mean_time_between_packets'] / group['expected_time_between_packets'], marker='o', label=f'{station}, {transfer}')
 
 # Add the ideal line
-plt.plot(hz_setting, expected_time, color='red', linestyle='--', label='Ideal')
+plt.axhline(y=1, color='red', linestyle='--', label='Ideal')
 
 
 plt.grid(True)
